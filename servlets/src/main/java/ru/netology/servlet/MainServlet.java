3546ee7448f956e665faf8d5e51f9083047d377a
SadovskiiAleks <alekssadovskij@yandex.ru>
2023-06-23 17:50:14 +0300

    add feature/di-annotation

--- servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -2,18 +2,18 @@ package ru.netology.servlet;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.netology.controller.PostController;
-import ru.netology.repository.PostRepository;
-import ru.netology.service.PostService;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import static ru.netology.servlet.MethodOfRequest.*;
+
 public class MainServlet extends HttpServlet {
 
     private static final String post = "/api/posts";
     private static final String postWithParam = "/api/posts/\\d+";
-    private PostController controller;
+    private static final PostController controller = null;
 
     @Override
     public void init() {
@@ -27,23 +27,23 @@ public class MainServlet extends HttpServlet {
         try {
             String path = req.getRequestURI();
             String method = req.getMethod();
-            Long id = 0l;
+            Long id;
             // primitive routing
-            if (method.equals(MethodOfRequest.GET.toString()) && path.equals(post)) {
+            if (method.equals(GET.toString()) && path.equals(post)) {
                 controller.all(resp);
                 return;
             }
-            if (method.equals(MethodOfRequest.GET.toString()) && path.matches(postWithParam)) {
+            if (method.equals(GET.toString()) && path.matches(postWithParam)) {
                 // easy way
                 id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
                 controller.getById(id, resp);
                 return;
             }
-            if (method.equals(MethodOfRequest.POST.toString()) && path.equals(post)) {
+            if (method.equals(POST.toString()) && path.equals(post)) {
                 controller.save(req.getReader(), resp);
                 return;
             }
-            if (method.equals(MethodOfRequest.DELETE.toString()) && path.matches(postWithParam)) {
+            if (method.equals(DELETE.toString()) && path.matches(postWithParam)) {
                 // easy way
                 id = Long.parseLong(path.substring(path.lastIndexOf("/") + 1));
                 controller.removeById(id, resp);
