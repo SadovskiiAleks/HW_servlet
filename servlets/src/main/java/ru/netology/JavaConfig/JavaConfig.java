60683c967038cef2d08ecb4c3bd73003f84c62dc
SadovskiiAleks <alekssadovskij@yandex.ru>
2023-06-23 15:32:39 +0300

    add feature/di-java

---
+++ servlets/src/main/java/ru/netology/JavaConfig/JavaConfig.java
@@ -0,0 +1,29 @@
+package ru.netology.JavaConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.controller.PostController;
+import ru.netology.repository.PostRepository;
+import ru.netology.repository.PostRepositoryStubImpl;
+import ru.netology.service.PostService;
+
+@Configuration
+public class JavaConfig {
+
+    @Bean
+    public PostController postController(PostService postService){
+        return new PostController(postService);
+    }
+
+    @Bean
+    public PostService postService(PostRepository postRepository){
+        return new PostService(postRepository);
+    }
+
+    @Bean
+    public PostRepository postRepository(){
+        return new PostRepositoryStubImpl();
+    }
+
+
+}
---
+++ servlets/src/main/java/ru/netology/repository/PostRepositoryStubImpl.java
@@ -0,0 +1,40 @@
+package ru.netology.repository;
+import ru.netology.model.Post;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+
+public class PostRepositoryStubImpl implements PostRepository {
+    private ConcurrentHashMap<Long, Post> mapOfPost = new ConcurrentHashMap<>();
+    private AtomicLong lastId = new AtomicLong();
+
+    public PostRepositoryStubImpl() {
+        lastId.set(0l);
+    }
+
+    public List<Post> all() {
+        return mapOfPost.values().stream().collect(Collectors.toList());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(mapOfPost.get(id));
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0l) {
+            post.setId(lastId.incrementAndGet());
+        } else if (!mapOfPost.containsKey(post.getId())) {
+            post.setId(lastId.incrementAndGet());
+        }
+        mapOfPost.put(post.getId(), post);
+        return post;
+    }
+
+    public void removeById(long id) {
+        mapOfPost.remove(id);
+    }
+}
